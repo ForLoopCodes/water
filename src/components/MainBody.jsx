757103d9dae2e09ec1758c1fe86f2435dd2e548a
MeetPatel1706 <meetnp1706@gmail.com>
2022-10-21 12:41:11 +0530

    once again?

--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -1,11 +1,47 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 export default function MainBody() {
   return (
-    <div>
+    <Router>
       <div className="mainbody">
-        <center>main body.</center>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/water">Home</Link>
+            </li>
+            <li>
+              <Link to="/water/product-page-555">Product</Link>
+            </li>
+            <li>
+              <Link to="/water/product-details-9999">Product Details</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/product-page-555" element={<Product />} />
+
+          <Route path="/product-details-9999" element={<ProductDetail />} />
+
+          <Route path="/" element={<Home />} />
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
+}
+
+function Home() {
+  const currentPathName = window.location.href;
+  return <h2>{currentPathName}</h2>;
+}
+
+function Product() {
+  const currentPathName = window.location.href;
+  return <h2>{currentPathName}</h2>;
+}
+
+function ProductDetail() {
+  const currentPathName = window.location.href;
+  return <h2>{currentPathName}</h2>;
 }
